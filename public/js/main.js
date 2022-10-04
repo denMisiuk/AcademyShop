13a4bead9c0fce19fcc44e5c80cb8eaf26c64ec8
Denis Misiuk <denis.misiuk@mail.ru>
2022-10-04 19:46:22 +0300

    +somecommit

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/***/ (function() {
+
+eval("//const sum = require(\"./module/sum.js\");\r\n//const ale = require(\"./module/test\")\r\n\r\nppp = prompt('dddd', 'dd');\r\nsss = confirm('confirm');\r\nalert(ppp);\r\nalert(sss);\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n//console.log(sum(2, 10));\r\n\r\n//const button = document.querySelector('.menu__button');\r\n//const menuList = document.querySelector('.menu__list');\r\n//const menuLink = document.querySelectorAll('.menu__link');\r\n//const moveMenu = document.querySelector('.firm__body');\r\n\r\n\r\n\r\n\r\n//burger icon\r\n//button.addEventListener('click', function() {\r\n//   button.classList.toggle('menu__button-active');\r\n//   menuList.classList.toggle('menu__list-active');\r\n//   moveMenu.classList.toggle('plus-menu');\r\n//})\r\n\r\n\r\n//toggle navigation\r\n//for (let links of menuLink) {\r\n//   links.addEventListener('click', function() {\r\n//      for (let link of menuLink) {\r\n//         link.classList.remove('menu__link-active')\r\n//      };\r\n//      links.classList.toggle('menu__link-active')\r\n//   })\r\n//}\r\n\r\n\n\n//# sourceURL=webpack://gulpcodequest/./src/js/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/main.js"]();
+/******/ 	
+/******/ })()
+;
